48c343c021871977236ed90c60a22ee7c672a7c8
Sanjivani Sharma <sanjivanisharma01@gmail.com>
2025-11-23 00:13:45 +0530

    fix: added try-catch for queries

--- db/queries.js
+++ db/queries.js
@@ -1,39 +1,64 @@
 const pool = require('./pool');
 
-async function getAllLinks() {
-    const query = 'SELECT * FROM links ORDER BY created_at DESC';
-    const { rows } = await pool.query(query);
-    return rows;
+async function getAllLinks(user_id) {
+    try {
+        const query = 'SELECT * FROM links WHERE user_id = $1 ORDER BY created_at DESC';
+        const { rows } = await pool.query(query, [user_id]);
+        return rows;
+    } catch (error) {
+        console.error('Error fetching links:', error);
+        throw error;
+    }
 }
 
 async function getLinkByCode(code) {
-    const query = 'SELECT * FROM links WHERE code = $1';
-    const { rows } = await pool.query(query, [code]);
-    return rows[0];
+    try {
+        const query = 'SELECT * FROM links WHERE code = $1';
+        const { rows } = await pool.query(query, [code]);
+        return rows[0];
+    } catch (error) {
+        console.error('Error fetching link by code:', error);
+        throw error;
+    }
 }
 
 async function createLink({code, target_url, user_id}) {
-    const query = `
-    INSERT INTO links (code, target_url, click_count, user_id, created_at, timestamp) 
-    VALUES ($1, $2, 0, $3, ${Date.now}, ${Date.now()}) 
-    RETURNING *`;
-    const { rows } = await pool.query(query, [code, target_url, user_id]);
-    return rows[0];
+    try {
+        const query = `
+        INSERT INTO links (code, target_url, click_count, user_id, created_at, timestamp) 
+        VALUES ($1, $2, 0, $3, ${Date.now()}, ${Date.now()}) 
+        RETURNING *`;
+        const { rows } = await pool.query(query, [code, target_url, user_id]);
+        return rows[0];
+    } catch (error) {
+        console.error('Error creating link:', error);
+        throw error;
+    }
 }
 
 async function deleteLink(code) {
-    const query = 'DELETE FROM links WHERE code = $1';
-    const result = await pool.query(query, [code]);
-    return result
+    try {
+        const query = 'DELETE FROM links WHERE code = $1';
+        const { rowCount } = await pool.query(query, [code]);
+        return rowCount;
+    } catch (error) {
+        console.error('Error deleting link:', error);
+        throw error;
+    }
 }
 
 async function incrementClick(code) {
-    const query = `
-    UPDATE LINKS
-    SET click_count = click_count + 1, timestamp = ${Date.now()}
-    WHERE code = $1`
-    const result = await pool.query(query, [code])
-    return result
+    try {
+        const query = `
+        UPDATE LINKS
+        SET click_count = click_count + 1, timestamp = ${Date.now()}
+        WHERE code = $1`
+        const result = await pool.query(query, [code])
+        return result
+    } catch (error) {
+        console.error('Error incrementing click count:', error);
+        throw error;
+    }
 }
 
 module.exports = {
